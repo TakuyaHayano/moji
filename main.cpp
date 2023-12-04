be441ea74b9f57d2d308ac8689f309d9983233fa
拓也 早野 <zelkova0821@outlook.com>
2023-12-04 10:24:53 +0900

    correct display part

--- main.cpp
+++ main.cpp
@@ -18,7 +18,6 @@ int main(void)
 	//ファイルからメモリへ読み込む
 	char* fig = (char*)malloc(fileSize);
 	fread(fig, sizeof(char), fileSize, fp);
-	printf("%d\n", fileSize);
 
 	fclose(fp);
 
@@ -42,20 +41,19 @@ int main(void)
 	if (fileSize % col != 0) {
 		puts("最後の行を改行していません。");
 	}
-	printf("row:%d\n", row);
-	printf("col:%d\n", col);
 
 	//表示する
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < col; j++) {
 			int idx = i * col + j;
-			if (fig[idx] == 1) {
+			if (fig[idx] == '1') {
 				printf("＊");
 			}
-			else if (fig[idx] == 0) {
+			else if (fig[idx] == '0') {
 				printf("　");
 			}
 		}
+		putchar('\n');
 	}
 
 
